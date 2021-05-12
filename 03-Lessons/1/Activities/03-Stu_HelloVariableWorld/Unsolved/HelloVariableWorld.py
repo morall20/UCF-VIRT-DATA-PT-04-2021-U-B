0d9ffc227fd6a7397408af4e585aed863294409f
morall20 <82338072+morall20@users.noreply.github.com>
2021-05-12 10:39:35 -0400

    update

--- 03-Lessons/1/Activities/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
+++ 03-Lessons/1/Activities/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -1,29 +1,29 @@
 # Create a variable called 'name' that holds a string
-
+name = "Mark"
 
 # Create a variable called 'country' that holds a string
-
+country = "US"
 
 # Create a variable called 'age' that holds an integer
-
+age = 23
 
 # Create a variable called 'hourly_wage' that holds an integer
-
+hourly_wage = 12.50
 
 # Calculate the daily wage for the user
-
+daily_wage = hourly_wage * 8
 
 # Create a variable called 'satisfied' that holds a boolean
-
+satisfied = True
 
 # Print out "Hello <name>!"
-
+print("Hello" + name + '!')
 
 # Print out what country the user entered
-
+print(country)
 
 # Print out the user's age
-
+print(age) 
 
 # With an f-string, print out the daily wage that was calculated
 
--- 03-Lessons/1/Activities/05-Stu_GroceryList/Unsolved/grocery_list.py
+++ 03-Lessons/1/Activities/05-Stu_GroceryList/Unsolved/grocery_list.py
@@ -1,9 +1,12 @@
 # Create a Python list to store your grocery list
-
+grocery_list = ["Milk", "Cheese", "Wine", "Sandwich"]
 # Print the grocery list
-
+print(grocery_list)
 # Change "Peanut Butter" to "Almond Butter" and print out the updated list
-
+grocery_list[1] = "Tomatoes"
 # Remove "Jelly" from grocery list and print out the updated list
-
+grocery_list.remove("Wine")
+grocery_list.pop(2)
 # Add "Coffee" to grocery list and print the updated list
+grocery_list.append("Coffee")
+print(grocery_list)
